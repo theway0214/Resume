3110e88a2d49c1e9d2f95c60ddbbc7b41b5f6538
xyq <1678028821@qq.com>
2025-09-29 10:12:42 +0800

    修改

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // 设置GitHub Pages部署的基础路径
+  // 如果你的仓库名是 'gd'，则设置为 '/gd/'
+  // 如果是部署到用户页面（username.github.io），则设置为 '/'
+  base: '/Resume/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
